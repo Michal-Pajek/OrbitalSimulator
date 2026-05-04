7b57d8cee4b87b85ae6c6d825437c396445e8160
Michał Pajek <michal.pajek@gmail.com>
2026-05-04 18:12:41 +0200

    delete Scenario.cpp

--- src/runner/SimulationRunner.cpp
+++ src/runner/SimulationRunner.cpp
@@ -6,12 +6,12 @@
 
 void SimulationRunner::runAndRecord(Recorder& recorder, const Scenario& scenario)
 {
-	const auto steps{ scenario.getStepCount() };
-	const auto& bodies{ scenario.getBodies() };
+	const auto steps{ scenario.m_stepCount };
+	const auto& bodies{ scenario.m_bodies };
 	if (!recorder.beginRecording()) {
 		throw std::runtime_error("Could not open output file.");
 	}
-	Simulation simulation{ bodies, scenario.getTimeStep() };
+	Simulation simulation{ bodies, scenario.m_timeStep };
 	recorder.recordInitialState(bodies);
 	for (unsigned int i{}; i < steps;) {
 		simulation.step();
