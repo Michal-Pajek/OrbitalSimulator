f3ada0191df72fe6adf9a907d8217924d7a10317
Michał Pajek <michal.pajek@gmail.com>
2026-08-26 11:20:38 +0200

    Improve ExceptionHandler tests

--- src/app/tests/ExceptionHandlerTests.cpp
+++ src/app/tests/ExceptionHandlerTests.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
 #include <stdexcept>
 
 #include "app/ExceptionHandler.hpp"
@@ -15,12 +17,26 @@ namespace
 
     TEST(ExceptionHandlerTests, ExecuteCatchesStdException)
     {
+        std::ostringstream output{};
+        auto* originalBuffer{ std::cout.rdbuf(output.rdbuf()) };
+
         EXPECT_NO_THROW(ExceptionHandler::execute([] { throw std::runtime_error{ "Test exception" }; }));
+
+        std::cout.rdbuf(originalBuffer);
+
+        EXPECT_EQ(output.str(), "ERROR: Test exception\n");
     }
 
     TEST(ExceptionHandlerTests, ExecuteCatchesUnknownException)
     {
+        std::ostringstream output{};
+        auto* originalBuffer{ std::cout.rdbuf(output.rdbuf()) };
+
         EXPECT_NO_THROW(ExceptionHandler::execute([] { throw 42; }));
+
+        std::cout.rdbuf(originalBuffer);
+
+        EXPECT_EQ(output.str(), "ERROR: Unknown exception\n");
     }
 
 } // anonymous namespace
